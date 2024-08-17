9d74c8281ca52d58528977eea9bb0acce3787ca3
ImScalf <139153733+ImScalf@users.noreply.github.com>
2024-08-17 23:26:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -31,12 +31,12 @@ app.get('/', (req, res) => {
   res.send('YaY Your Bot Status Changed✨');
 });
 app.listen(port, () => {
-  console.log(`🔗 Listening to RTX: http://localhost:${port}`);
-  console.log(`🔗 Powered By RTX`);
+  console.log(`🔗 Listening to TXRP: http://localhost:${port}`);
+  console.log(`🔗 Powered By TXRP`);
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = ["TXRP","TXRP"];
 
 
 let currentIndex = 0;
